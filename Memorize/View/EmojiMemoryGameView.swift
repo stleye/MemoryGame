9f88f9aa90f563869fef91850fa6bfba0263af3e
stleye <stleye@gmail.com>
2022-07-09 10:50:39 -0300

    Added ViewModifier Cardify

--- Memorize/View/EmojiMemoryGameView.swift
+++ Memorize/View/EmojiMemoryGameView.swift
@@ -40,32 +40,23 @@ struct CardView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Pie(startAngle: Angle(degrees: 0-90),
-                        endAngle: Angle(degrees: 110-90))
-                        .padding(5).opacity(0.5)
-                    Text(card.content).font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
-                }
+                Pie(startAngle: Angle(degrees: 0-90),
+                    endAngle: Angle(degrees: 110-90))
+                    .padding(5).opacity(0.5)
+                Text(card.content).font(font(in: geometry.size))
             }
+            .cardify(isFaceUp: card.isFaceUp)
         }
     }
 
     private func font(in size: CGSize) -> Font {
         Font.system(size: min(size.height, size.width) * DrawingConstants.fontScale)
     }
-    
+
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.7
     }
+
 }
 
 
